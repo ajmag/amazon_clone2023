9effb3423acb13dd71918867bcacc8b24b1b602b
Anthony <anthonyjm.company@gmail.com>
2023-12-06 11:20:42 -0500

    Created main page & Checkout page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAE9zLqKBhX6_D7c5WIWelXMo4ue5gn_B4",
+    authDomain: "clone-of-de663.firebaseapp.com",
+    projectId: "clone-of-de663",
+    storageBucket: "clone-of-de663.appspot.com",
+    messagingSenderId: "438369644630",
+    appId: "1:438369644630:web:87f13d8adefe28f83bcbf7",
+    measurementId: "G-81ETCLXLSL"
+  };
